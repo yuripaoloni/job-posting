385c8afb771c98f5fd25e0956bc7c5d0e2acdd0f
yuripaoloni <yuri.paoloni3@gmail.com>
2022-05-19 21:16:06 +0200

    style: soft skill page

--- client/src/pages/user/SoftSkill.tsx
+++ client/src/pages/user/SoftSkill.tsx
@@ -7,6 +7,7 @@ import {
   NavLink,
   Row,
   Col,
+  Container,
 } from "design-react-kit";
 
 type SoftSkillProps = {};
@@ -19,41 +20,48 @@ const SoftSkill = (props: SoftSkillProps) => {
   };
 
   return (
-    <Row className="justify-content-center py-3">
-      <Col xs={3}>
-        <Nav tabs>
-          {Array(14)
-            .fill(0)
-            .map((i, index) => (
-              <NavItem className="col-7">
-                <NavLink
-                  active={activeTab === index}
-                  onClick={() => toggle(index)}
-                >
-                  Domanda {index + 1}
-                </NavLink>
-              </NavItem>
-            ))}
-        </Nav>
-      </Col>
-      <Col xs={7}>
-        <TabContent activeTab={activeTab}>
-          {Array(14)
-            .fill(0)
-            .map((i, index) => (
-              <TabPane tabId={index} className="p-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </TabPane>
-            ))}
-        </TabContent>
-      </Col>
-    </Row>
+    <Container fluid className="py-4">
+      <Row className="justify-content-center">
+        <h4>Soft skill</h4>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={2}>
+          <Nav pills vertical>
+            {Array(14)
+              .fill(0)
+              .map((i, index) => (
+                <NavItem key={index} className="col text-center ">
+                  <NavLink
+                    role="button"
+                    active={activeTab === index}
+                    onClick={() => toggle(index)}
+                  >
+                    {index + 1}
+                  </NavLink>
+                </NavItem>
+              ))}
+          </Nav>
+        </Col>
+        <Col md={9} xs={10}>
+          <TabContent activeTab={activeTab}>
+            {Array(14)
+              .fill(0)
+              .map((i, index) => (
+                <TabPane key={index} tabId={index} className="p-3">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </TabPane>
+              ))}
+          </TabContent>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 

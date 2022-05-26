5d9e0d4e4dabdd3ab763da53e73ab36489577603
yuripaoloni <yuri.paoloni3@gmail.com>
2022-05-26 18:00:34 +0200

    perf: added maxLength check on DTOs

--- server/src/dtos/auth.dto.ts
+++ server/src/dtos/auth.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class LoginDto {
   @IsString()
+  @MaxLength(255)
   public username: string;
 
   @IsString()
+  @MaxLength(255)
   public password: string;
 }

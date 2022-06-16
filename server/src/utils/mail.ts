101194c009d477253d3bf3dc67b243dcfad595be
yuripaoloni <yuri.paoloni3@gmail.com>
2022-06-16 20:44:31 +0200

    perf: job offer manager in 'bcc' field of interview email

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -1,12 +1,13 @@
 import { EMAIL } from '@/config';
 import transporter from '@/config/mail';
 
-const sendEmail = async (to: string, subject: string, text: string) => {
+const sendEmail = async (to: string, subject: string, text: string, bcc?: string) => {
   const mailOptions = {
     from: EMAIL,
     to,
     subject,
     text,
+    bcc,
   };
 
   await transporter.sendMail(mailOptions);

50d706d4dbb4291c4b0e4b6ab19aacb66a220293
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-05 10:32:05 +0200

    test: test draft

--- server/src/tests/auth.test.ts
+++ server/src/tests/auth.test.ts
@@ -1,12 +1,13 @@
-import bcrypt from 'bcrypt';
 import request from 'supertest';
 import { createConnection, getRepository } from 'typeorm';
-import App from '@/app';
-import { dbConnection } from '@databases';
-import { CreateUserDto } from '@dtos/users.dto';
-import AuthRoute from '@routes/auth.route';
+import App from '../app';
+import AuthRoute from '../routes/auth.route';
+import { dbConnection } from '../databases';
+import { LoginDto } from '../dtos/auth.dto';
+import { UtenteEntity } from '../entities/utente.entity';
+import { Utente } from '../interfaces/utente.interface';
 
-beforeAll(async () => {
+beforeEach(async () => {
   await createConnection(dbConnection);
 });
 
@@ -14,56 +15,58 @@ afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
 });
 
-describe('Testing Auth', () => {
-  describe('[POST] /signup', () => {
-    it('response should have the Create userData', async () => {
-      const userData: CreateUserDto = {
-        email: 'test@email.com',
-        password: 'q1w2e3r4!',
-      };
-
-      const authRoute = new AuthRoute();
-      const users = authRoute.authController.authService.users;
-      const userRepository = getRepository(users);
-
-      userRepository.findOne = jest.fn().mockReturnValue(null);
-      userRepository.save = jest.fn().mockReturnValue({
-        id: 1,
-        email: userData.email,
-        password: await bcrypt.hash(userData.password, 10),
-      });
-
-      const app = new App([authRoute]);
-      return request(app.getServer()).post(`${authRoute.path}signup`).send(userData).expect(201);
-    });
-  });
-
+describe('/aut', () => {
   describe('[POST] /login', () => {
     it('response should have the Set-Cookie header with the Authorization token', async () => {
-      const userData: CreateUserDto = {
-        email: 'test@email.com',
+      const userData: LoginDto = {
+        username: 'mario.rossi',
         password: 'q1w2e3r4!',
       };
 
       const authRoute = new AuthRoute();
-      const users = authRoute.authController.authService.users;
-      const userRepository = getRepository(users);
+      const utenteRepository = getRepository(UtenteEntity);
 
-      userRepository.findOne = jest.fn().mockReturnValue({
-        id: 1,
-        email: userData.email,
-        password: await bcrypt.hash(userData.password, 10),
+      utenteRepository.findOne = jest.fn().mockReturnValue({
+        cf: 'abc123',
+        nome: 'Mario',
+        cognome: 'Rossi',
+        email: 'mario.rossi@unicam.it',
+        username: userData.username,
+        annoPrimaOccupazione: null,
+        annoIngressoUnicam: null,
+        preparazione: null,
       });
 
       const app = new App([authRoute]);
       return request(app.getServer())
-        .post(`${authRoute.path}login`)
+        .post(`${authRoute.path}/login`)
         .send(userData)
         .expect('Set-Cookie', /^Authorization=.+/);
     });
   });
+  // describe('[POST] /signup', () => {
+  //   it('response should have the Create userData', async () => {
+  //     const userData: CreateUserDto = {
+  //       email: 'test@email.com',
+  //       password: 'q1w2e3r4!',
+  //     };
 
-  // describe('[POST] /logout', () => {
+  //     const authRoute = new AuthRoute();
+  //     const users = authRoute.authController.authService.users;
+  //     const userRepository = getRepository(users);
+
+  //     userRepository.findOne = jest.fn().mockReturnValue(null);
+  //     userRepository.save = jest.fn().mockReturnValue({
+  //       id: 1,
+  //       email: userData.email,
+  //       password: await bcrypt.hash(userData.password, 10),
+  //     });
+
+  //     const app = new App([authRoute]);
+  //     return request(app.getServer()).post(`${authRoute.path}signup`).send(userData).expect(201);
+  //   });
+  // });
+  // describe('[GET] /logout', () => {
   //   it('logout Set-Cookie Authorization=; Max-age=0', async () => {
   //     const authRoute = new AuthRoute();
   //     const app = new App([authRoute]);
@@ -73,4 +76,6 @@ describe('Testing Auth', () => {
   //       .expect('Set-Cookie', /^Authorization=\;/);
   //   });
   // });
+  // describe('[GET] /validate', () => {});
+  // describe('[GET] /userType/:userType', () => {});
 });

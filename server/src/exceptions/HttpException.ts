5197e4d0b137170d88f7c3b4fe2f96b1a07504df
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-11 13:02:48 +0200

    perf: customMessage in httpException

--- server/src/exceptions/HttpException.ts
+++ server/src/exceptions/HttpException.ts
@@ -1,10 +1,10 @@
 export class HttpException extends Error {
   public status: number;
-  public message: string;
+  public customMessage: string;
 
   constructor(status: number, message: string) {
     super(message);
     this.status = status;
-    this.message = message;
+    this.customMessage = message;
   }
 }
